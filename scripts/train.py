22106b6fd118b27b8d0161b6c6da32cba59c6f75
cpa2001 <cpa2001@connect.hku.hk>
2026-03-17 23:22:12 -0700

    add comments

--- scripts/train.py
+++ scripts/train.py
@@ -77,7 +77,7 @@ def init_wandb(config: _config.TrainConfig, *, resuming: bool, log_code: bool = 
 
 
 def _load_weights_and_validate(loader: _weight_loaders.WeightLoader, params_shape: at.Params) -> at.Params:
-    """Loads and validates the weights. Returns a loaded subset of the weights."""
+    """Loads and validates overlapping weights. Returns a subset to merge into initialized params."""
     loaded_params = loader.load(params_shape)
 
     flat_expected = traverse_util.flatten_dict(params_shape)
