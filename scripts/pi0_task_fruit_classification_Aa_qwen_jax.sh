4c6b1c82c71047c94e601f295b3d068ec5d1fe07
Bojack-BJ <1243071552@qq.com>
2026-04-09 05:39:06 -0700

    local changes in config file

--- scripts/pi0_task_fruit_classification_Aa_qwen_jax.sh
+++ scripts/pi0_task_fruit_classification_Aa_qwen_jax.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-export CUDA_VISIBLE_DEVICE=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 source /root/Users/miniconda3/etc/profile.d/conda.sh
 conda activate pi0_suzhou
 
@@ -9,18 +9,21 @@ export PYTHONPATH="$REPO/src:$REPO/packages/openpi-client/src:$PYTHONPATH"
 
 cfg=fruit_classification_Aa_qwen
 
+
 # cp /home/liyang/.cache/openpi/big_vision/* /root/.cache/openpi/big_vision/
 
 exp="fruit_classification_Aa_qwen_jax"
 
 export HF_LEROBOT_HOME='/root/Users/dataset/lerobot_home'
 export HF_DATASETS_CACHE="/root/Users/.cache/"
-export WANDB_MODE=offline
+export WANDB_MODE=online
 
 cd /root/Users/lixiaotong/openpi
 
 # python scripts/compute_norm_stats.py --config-name "$exp"
-  
+
+HF_HUB_OFFLINE=1 
+TRANSFORMERS_OFFLINE=1 
 # XLA_PYTHON_CLIENT_PREALLOCATE=false \
 # XLA_PYTHON_CLIENT_ALLOCATOR=platform \
 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 \
