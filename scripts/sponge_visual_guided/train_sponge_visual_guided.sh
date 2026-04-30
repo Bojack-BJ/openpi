c4d35312ffb6e2aa591512fed9e05bd4aa63b3a3
Bojack-BJ <1243071552@qq.com>
2026-04-30 01:49:46 -0700

    local adjustment

--- scripts/sponge_visual_guided/train_sponge_visual_guided.sh
+++ scripts/sponge_visual_guided/train_sponge_visual_guided.sh
@@ -20,7 +20,7 @@ esac
 exp=${EXP_NAME:-$cfg}
 repo=${REPO:-/root/Users/lixiaotong/openpi}
 
-export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0,1,2,3}
+export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0,1,2,3,4,5,6,7}
 export HF_LEROBOT_HOME=${HF_LEROBOT_HOME:-/root/Users/dataset/lerobot_home}
 export HF_DATASETS_CACHE=${HF_DATASETS_CACHE:-/root/Users/.cache/}
 export WANDB_MODE=${WANDB_MODE:-online}
@@ -32,12 +32,12 @@ conda activate "${CONDA_ENV:-pi0_suzhou}"
 cd "$repo"
 
 # First run only, if assets/norm stats do not exist yet:
-# python scripts/compute_norm_stats.py "$cfg" --num-workers 8
+python scripts/compute_norm_stats.py --config-name "$cfg" --num-workers 64
 
 XLA_PYTHON_CLIENT_MEM_FRACTION=${XLA_PYTHON_CLIENT_MEM_FRACTION:-0.9} \
 python scripts/train.py "$cfg" \
   --exp-name "$exp" \
   --project_name "${PROJECT_NAME:-umi-openpi}" \
-  --fsdp_devices "${FSDP_DEVICES:-4}" \
+  --fsdp_devices "${FSDP_DEVICES:-8}" \
   --jax-compilation-cache-dir "${JAX_COMPILATION_CACHE_DIR:-/root/Users/.cache/jax/openpi}" \
   --overwrite
